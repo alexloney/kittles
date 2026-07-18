ca1d8d66c6cb7e6016348dc790556ba310dbde2f
Alex Loney <loney.alex@gmail.com>
2026-07-17 17:55:40 -0700

    Adding watcher to watch for new files and restart the serving to bypass an issue preventing new posts from showing up.

--- .eleventy.js
+++ .eleventy.js
@@ -5,8 +5,14 @@ module.exports = function(eleventyConfig) {
   // Pass through static assets
   eleventyConfig.addPassthroughCopy("src/assets");
 
-  // Watch for CSS changes during development
+  // Watch for CSS changes during development  
   eleventyConfig.addWatchTarget("src/assets/css");
+  
+  // Disable JavaScript dependency tracking for cleaner rebuilds
+  eleventyConfig.setWatchJavaScriptDependencies(false);
+  
+  // Reduce throttle time for faster rebuilds
+  eleventyConfig.setWatchThrottleWaitTime(100);
 
   eleventyConfig.addFilter("readableDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("MMMM dd, yyyy");
@@ -19,6 +25,13 @@ module.exports = function(eleventyConfig) {
       .sort((a, b) => a.data.navOrder - b.data.navOrder);
   });
 
+  // Explicitly define lifestory collection to ensure it updates in watch mode
+  eleventyConfig.addCollection("lifestory", function(collectionApi) {
+    return collectionApi
+      .getFilteredByTag("lifestory")
+      .sort((a, b) => a.date - b.date);
+  });
+
   return {
     dir: {
       input: "src",
@@ -28,6 +41,13 @@ module.exports = function(eleventyConfig) {
     },
     // Set default template language
     markdownTemplateEngine: "njk",
-    htmlTemplateEngine: "njk"
+    htmlTemplateEngine: "njk",
+    // Disable incremental builds - forces full rebuild on file changes
+    // This ensures collections update when new posts are added
+    incremental: false,
+    // Watch options for better hot reload
+    watchOptions: {
+      ignored: ['_site/**']
+    }
   };
 };
